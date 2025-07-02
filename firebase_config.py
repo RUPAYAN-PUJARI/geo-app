273cf1ffe7396401f46e70b8cf4e8fa4a4110f79
Rupayan Pujari <rupayanpujari308@gmail.com>
2025-07-02 21:19:27 +0530

    updated

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,20 @@
+import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("attendance-1a9df-firebase-adminsdk-fbsvc-401b975baf.json")
+# Get the base64-encoded JSON string from the environment variable
+base64_key = os.environ.get("FIREBASE_CREDENTIALS_BASE64")
+
+if not base64_key:
+    raise RuntimeError("FIREBASE_CREDENTIALS_BASE64 environment variable is missing.")
+
+# Decode and load the credentials
+decoded_json = base64.b64decode(base64_key).decode("utf-8")
+service_account_info = json.loads(decoded_json)
+
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
